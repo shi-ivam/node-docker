8b48e1c3570aaa1d2ab83002681016d675691778
Shivam Kumar <savagedeveloper795@gmail.com>
2022-06-23 18:37:26 +0530

    Changes

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ const express = require('express');
 const app = express()
 const mongoose = require('mongoose');
 
+let status = "offline";
 mongoose.connect('mongodb://root:root@mongo:27017', { useNewUrlParser: true }).then(() => {
     console.log('Connected to MongoDB');
+    status = "online";
 })
 .catch(() => {
     console.log("Failed to Connect")
+    status = "failed";
 })
 
 app.get('/',(req,res) => {
